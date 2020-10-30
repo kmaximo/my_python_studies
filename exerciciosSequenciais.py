289b1d863ca0ebd755da5bd84a0acef6de6427c0
Klinsmann Maximo <klinsmann_max@hotmail.com>
2020-10-29 23:45:09 -0300

    Create exerciciosSequenciais.py

---
+++ exerciciosSequenciais.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 29 23:30:07 2020
+
+@author: Maximo
+"""
+
+#EstruturaSequencial
+#1º Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+#print("Hello Word!!!")
+
+#2º Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+#print(input("Informe um numero: "))
+
+#3º Faça um Programa que peça dois números e imprima a soma.
+
+# n1=int(input("Informe o primeiro numero: "))
+# n2=int(input("Informe o segundo numero: "))
+
+# print("A Soma foi "+str(n1+n2))
+
+#4º Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+# n1=int(input("Informe a primeira nota: "))
+# n2=int(input("Informe a segunda nota: "))
+# n3=int(input("Informe a terceira nota: "))
+# n4=int(input("Informe a quarta nota: "))
+
+# print("A média é "+str((n1+n2+n3+n4)/4))
+
+#5º 
