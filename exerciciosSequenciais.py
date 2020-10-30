f6f6c26855e39cd15cd7268c2a11c83e39958867
Klinsmann Maximo <klinsmann_max@hotmail.com>
2020-10-30 15:13:50 -0300

    Update exerciciosSequenciais.py

--- exerciciosSequenciais.py
+++ exerciciosSequenciais.py
@@ -65,4 +65,4 @@ Created on Thu Oct 29 23:30:07 2020
 #9º Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
 #C = 5 * ((F-32) / 9).
 
-temperatura
+#temperatura
