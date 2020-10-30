a4acd57e6d2297a44d121ae56734b4f212be27b1
Klinsmann Maximo <klinsmann_max@hotmail.com>
2020-10-29 23:09:04 -0300

    Create Aula1Python.py

---
+++ Aula1Python.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 29 23:00:55 2020
+
+@author: Maximo
+"""
+
+    
+n1 = int(input("Escolha o primerio numero: "))
+n2 = int(input("Escolha o segundo numero: "))
+   
+soma = n1+n2
+sub = n1-n2
+mult = n1*n2
+divf = n1/n2
+div = n1//n2
+    
+print("A soma foi "+str(soma)+" a subtração foi "+str(sub)+
+          " a multiplicação foi "+str(mult)+" a divisão foi "+str(divf)+" ou "+str(div))
+ 
