8cfe639212512a93d4601f80313dbf2294bf6e43
Klinsmann Maximo <klinsmann_max@hotmail.com>
2020-10-30 23:28:56 -0300

    Update exerciciosSequenciais.py

--- exerciciosSequenciais.py
+++ exerciciosSequenciais.py
@@ -4,39 +4,59 @@ Created on Thu Oct 29 23:30:07 2020
 
 @author: Maximo
 """
+def get_menu_choice():
+    def print_menu():       ## Your menu design here
+        print (30 * "-" , "Estrutura Sequencial" , 30 * "-")
+        print ("1º Faça um Programa que mostre a mensagem 'Alo mundo' na tela. ")
+        print ("2º Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].")
+        print ("3º Faça um Programa que peça dois números e imprima a soma.")
+        print ("4º Faça um Programa que peça as 4 notas bimestrais e mostre a média.")
+        print ("5º Faça um Programa que converta metros para centímetros.")
+        print ("5º Faça um Programa que converta metros para centímetros.")
+        print ("5º Faça um Programa que converta metros para centímetros.")
+        print ("5º Faça um Programa que converta metros para centímetros.")
+        print ("5º Faça um Programa que converta metros para centímetros.")
+        print ("5º Faça um Programa que converta metros para centímetros.")
+        print ("5º Faça um Programa que converta metros para centímetros.")
+        print (67 * "-")
+  
+    loop=True
+    while loop:          ## o laço só vai parar quando o loop = False
+        print_menu()    ## mostra menu
+        escolha = input("Escolha o nº do exercicio: ")
+        if escolha == '1':
+            #1º Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+            print("Hello Word!!!")
+            loop = False
+        elif escolha == '2':
+            print(input("Informe um numero: "))
+        elif escolha == '3':
+            n1=int(input("Informe o primeiro numero: "))
+            n2=int(input("Informe o segundo numero: "))
 
-#EstruturaSequencial
-#1º Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+            print("A Soma foi "+str(n1+n2))
+        elif escolha == '4':
+            n1=int(input("Informe a primeira nota: "))
+            n2=int(input("Informe a segunda nota: "))
+            n3=int(input("Informe a terceira nota: "))
+            n4=int(input("Informe a quarta nota: "))
+            
+            print("A média é ",(n1+n2+n3+n4)/4)
+        elif escolha == '5':
+            metros = float(input("Metros a ser convertidos: "))
+            
+            cent = metros * 1000
+            
+            print(str(cent)+"cm")            
+        else:
+            print("Opção errada.")
+            loop = False
+    return 
 
-#print("Hello Word!!!")
 
-#2º Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+print(get_menu_choice())
 
-#print(input("Informe um numero: "))
 
-#3º Faça um Programa que peça dois números e imprima a soma.
-
-# n1=int(input("Informe o primeiro numero: "))
-# n2=int(input("Informe o segundo numero: "))
-
-# print("A Soma foi "+str(n1+n2))
-
-#4º Faça um Programa que peça as 4 notas bimestrais e mostre a média.
-
-# n1=int(input("Informe a primeira nota: "))
-# n2=int(input("Informe a segunda nota: "))
-# n3=int(input("Informe a terceira nota: "))
-# n4=int(input("Informe a quarta nota: "))
-
-# print("A média é ",(n1+n2+n3+n4)/4)
-
-#5º Faça um Programa que converta metros para centímetros.
-
-# metros = float(input("Metros a ser convertidos: "))
-
-# cent = metros * 1000
-
-# print(str(cent)+"cm")
 
 #6º Faça um Programa que peça o raio de um círculo, calcule e mostre sua 
 
