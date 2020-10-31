1b1d8a912d453a30d63142c20ef455d8a583c5e8
Klinsmann Maximo <klinsmann_max@hotmail.com>
2020-10-30 23:44:36 -0300

    Update exerciciosSequenciais.py

--- exerciciosSequenciais.py
+++ exerciciosSequenciais.py
@@ -12,12 +12,10 @@ def get_menu_choice():
         print ("3º Faça um Programa que peça dois números e imprima a soma.")
         print ("4º Faça um Programa que peça as 4 notas bimestrais e mostre a média.")
         print ("5º Faça um Programa que converta metros para centímetros.")
-        print ("5º Faça um Programa que converta metros para centímetros.")
-        print ("5º Faça um Programa que converta metros para centímetros.")
-        print ("5º Faça um Programa que converta metros para centímetros.")
-        print ("5º Faça um Programa que converta metros para centímetros.")
-        print ("5º Faça um Programa que converta metros para centímetros.")
-        print ("5º Faça um Programa que converta metros para centímetros.")
+        print ("6º Faça um Programa que peça o raio de um círculo, calcule e mostre sua.")
+        print ("7º Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.")
+        print ("8º Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês, calcule e mostre o total do seu salário no referido mês.")
+        print ("9º Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius C = 5 * ((F-32) / 9).")
         print (67 * "-")
   
     loop=True
@@ -48,6 +46,25 @@ def get_menu_choice():
             cent = metros * 1000
             
             print(str(cent)+"cm")            
+        elif escolha == '6':
+            pi = 3.14
+            r = float(input("Digite a área do circulo em metros: "))
+            
+            a = pi * (r * r)
+            
+            print("A área do circulo é ",a,"m2")  
+        elif escolha == '7':
+            lado = float(input("Informe o valor do lado: "))
+            area = lado * lado
+            dobro = area * 2
+            print("A área é: ",area," e o dobro é :",dobro)            
+        elif escolha == '8':
+            valhora = float(input("Informe o valor da hora: "))
+            numhora = float(input("Informe o numeros de horas trabalhadas: "))
+            
+            print("Seu salário é de R$ ",numhora*valhora)            
+        elif escolha == '9':
+            print("")
         else:
             print("Opção errada.")
             loop = False
@@ -55,34 +72,3 @@ def get_menu_choice():
 
 
 print(get_menu_choice())
-
-
-
-#6º Faça um Programa que peça o raio de um círculo, calcule e mostre sua 
-
-# pi = 3.14
-# r = float(input("Digite a área do circulo em metros: "))
-
-# a = pi * (r * r)
-
-# print("A área do circulo é ",a,"m2")
-
-#7º Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
-
-# lado = float(input("Informe o valor do lado: "))
-# area = lado * lado
-# dobro = area * 2
-# print("A área é: ",area," e o dobro é :",dobro)
-
-#8º Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
-#Calcule e mostre o total do seu salário no referido mês.
-
-# valhora = float(input("Informe o valor da hora: "))
-# numhora = float(input("Informe o numeros de horas trabalhadas: "))
-
-# print("Seu salário é de R$ ",numhora*valhora)
-
-#9º Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
-#C = 5 * ((F-32) / 9).
-
-#temperatura
